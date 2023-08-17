47ecc1163ce2076964268fda4ecf515e1f4f17f1
Replit user <>
2023-08-17 20:24:39 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -9,14 +9,69 @@ Ejercicio 1: Encuentra el Elemento Mayor
 Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
-int findMax(int arr[], int size) { return 0; }
+int encontrar_elemento_mayor(int arr[], int tam){
+  if (tam <=0){
+    return -1;
+  }
+
+  int elemento_mayor = arr[0];
+  for (int i = 1; i < tam; i++){
+    if (arr[i] > elemento_mayor){
+      elemento_mayor = arr[i];
+    }
+  }
+  return elemento_mayor;
+}
+
+int main(){
+  int arreglo[] = {12, 39, 2, 9, 16};
+  int tam = sizeof(arreglo) / sizeof(arreglo[0]);
+
+  int resultado = encontrar_elemento_mayor(arreglo, tam);
+  printf("El elemento mayor en el arreglo es: %d\n", resultado);
+
+  return 0;
+}
 
 /*
 Ejercicio 2: Invertir un Arreglo
 Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
-void reverseArray(int arr[], int size) {}
+void invertir_arreglo(int arr[], int tam){
+  int inicio = 0;
+  int fin = tam - 1; 
+
+  while (inicio < fin){
+    int temp = arr[inicio];
+    arr[inicio] = arr[fin];
+    arr[fin] = temp;
+
+    inicio++;
+    fin--;
+  }
+}
+
+int main() {
+  int arreglo[] = {1, 2, 3, 4, 5};
+  int tam = sizeof(arreglo) / sizeof(arreglo[0]);
+
+  printf("Arreglo original: ");
+  for (int i = 0; i < tam; i++){
+    printf("%d", arreglo[i]);
+  }
+  printf("\n");
+
+  invertir_arreglo(arreglo, tam);
+
+  printf("Arreglo invertido: ");
+  for (int i = 0; i < tam; i++){
+    printf("%d", arreglo[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
 
 /*
 Ejercicio 3: Filtrar Números Pares
@@ -24,7 +79,49 @@ Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
-int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
+int* filtrar_pares(int arr[], int tam, int* nuevo_tam){
+  int num_pares  = 0;
+
+  for (int i = 0; i < tam; i++) {
+    if (arr[i] % 2 == 0) {
+      num_pares++;
+    }
+  }
+
+  int* arreglo_pares = (int*)malloc(num_pares * sizeof(int));
+  if (arreglo_pares == NULL) {
+    perror("Error en la asignacion de memoria");
+    exit(EXIT_FAILURE);
+  }
+
+  int index = 0;
+  for (int i = 0; i < tam; i++) {
+    if (arr[i] % 2 == 0) {
+      arreglo_pares[index] = arr[i];
+      index++;
+    }
+  }
+  *nuevo_tam = num_pares;
+  return arreglo_pares;
+}
+
+int main() {
+  int arreglo[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int tam = sizeof(arreglo) / sizeof(arreglo[0]);
+
+  int nuevo_tam;
+  int* arreglo_pares = filtrar_pares(arreglo, tam, &nuevo_tam);
+
+  printf("Arreglo original: ");
+  for (int i = 0; i < tam; i++) {
+    printf("%d ", arreglo[i]);
+  }
+  printf("\n");
+
+  free(arreglo_pares);
+
+  return 0;
+}
 
 /*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
