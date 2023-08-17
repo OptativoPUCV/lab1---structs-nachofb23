d38952b49e186607f205aa672591284251cf3828
Replit user <>
2023-08-17 20:28:42 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -129,8 +129,50 @@ Descripción: Escribe una función que tome dos arreglos
 ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
-void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-                       int result[]) {}
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int merged[]) {
+  int i = 0, j = 0, k = 0;
+
+  while (i < size1 && j < size2) {
+    if (arr1[i] < arr2[j]) {
+      merged[k] = arr1[i];
+      i++;
+    } else {
+      merged[k] = arr2[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < size1) {
+    merged[k] = arr1[i];
+    i++;
+    k++;
+  }
+
+  while (j < size2) {
+    merged[k] = arr2[j];
+    j++;
+    k++;
+  }
+
+int main() {
+  int arr[] = (1, 3, 5, 7, 9);
+  int size1 = sizeof(arr1) / sizeof(arr1[0]);
+
+  int arr2[] = {2, 4, 6, 8, 10};
+  int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+  int result[size1 + size2];
+
+  mergeSortedArrays(arr1, size1, arr2, size2, merged);
+
+  printf("Arreglo fusionado:\n");
+  for (int i = 0; i < size1 + size2; i++) {
+    printf("%d ", merged[i]);
+  }
+
+  return 0;
+}
 
 /*
 Ejercicio 5: Comprobación de Ordenación
