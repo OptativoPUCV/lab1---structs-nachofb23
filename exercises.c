4dcdcae1d0fdc8ca3907bce8905beb19d4dd16d0
Replit user <>
2023-08-18 01:59:14 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -18,8 +18,8 @@ int findMax(int arr[], int size) {
     for (int i = 1; i < size; i++) {
         if (arr[i] > mayor) {
             mayor = arr[i];
-        };
-    }
+        }
+    };
     return mayor;
 }
 
